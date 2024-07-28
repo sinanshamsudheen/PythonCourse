23fddf7e768187c7e14d8305e159b56e738d09de
sinan <sinanshamsudheen7@gmail.com>
2024-07-28 15:45:28 +0530

    arrays in java

---
+++ array.java
@@ -0,0 +1,19 @@
+
+import java.util.Arrays;
+
+public class array {
+    public static void main(String[] args) {
+        int[] marks = new int[3];
+        marks[0]=9;
+        marks[1]=99;
+        marks[2]=91;
+
+        System.out.println(marks[1]);
+        Arrays.sort(marks);
+        System.out.println(marks[1]);
+
+        String[][] name = {{"sinan","liza"},{"vysh","shankar"}};
+        System.out.println(name[0][1]);
+        System.out.println(name[1][1]);
+    }
+}
