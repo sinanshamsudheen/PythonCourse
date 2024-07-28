d0034b96930fca8b38dbaaee0f500042c038c0a7
sinan <sinanshamsudheen7@gmail.com>
2024-07-28 19:28:01 +0530

    conditional statements

---
+++ conditional.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+public class conditional {
+    public static void main(String[] args) {
+        int n= 3;
+        while(n!=0){
+            System.out.println(n);
+            n--;
+        }
+
+        if(n==0){
+            System.out.println("final n value is zero.");
+        }
+        else{
+            System.out.println("final n value is not zero.");
+        }
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter m value: ");
+        int m=sc.nextInt();
+
+        switch(m){
+            case 1 : System.out.println("hello world");
+                    break;
+            case 2 : System.out.println("hello ladies");
+                    break;
+            default: System.out.println("googoo gaagaa!!");
+                    break;
+        }
+
+        int[] arr = new int[100];
+        System.out.print("enter the limit: ");
+        int val=sc.nextInt();
+        System.out.print("enter the elements: ");
+        for(int i=0;i<val;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<val;i++){
+            System.out.println(arr[i]);
+        }
+
+
+    }
+}
