633de16ac5b6b267f784c35713b09cf158431bb6
sinan <sinanshamsudheen7@gmail.com>
2024-07-28 20:25:38 +0530

    methods & functions

---
+++ mthdsNfns.java
@@ -0,0 +1,12 @@
+public class mthdsNfns {
+    //Functions
+    public static int AddSum(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        //Methods
+        int a=4,b=7;
+        System.out.println(AddSum(a,b));
+    }
+    
+}
