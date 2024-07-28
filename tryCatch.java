ebfbd8f0ae351e28464ded973f78eb10de601ca8
sinan <sinanshamsudheen7@gmail.com>
2024-07-28 19:32:28 +0530

    try-catch

---
+++ tryCatch.java
@@ -0,0 +1,11 @@
+public class tryCatch {
+    public static void main(String[] args) {
+        int[] marks={2,3,4,5};
+        try {
+        } catch (Exception e) {
+            //do something after catching
+        }
+        System.out.println(marks[5]);
+        System.out.println("The name is Don.");
+    }
+}
