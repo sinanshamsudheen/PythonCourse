dd48f94cccb8838016cd62891447f5493cadc823
sinan <sinanshamsudheen7@gmail.com>
2024-07-28 16:15:59 +0530

    math and SCANNING

---
+++ math.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class math {
+    public static void main(String[] args) {
+        int a=3,b=7;
+        System.out.println(Math.max(a,b));
+        System.out.println((int)(Math.random()*1000));
+
+        //How to take input?
+        Scanner sc =new Scanner(System.in);
+
+        System.out.print("Input ur name: ");
+        String name=sc.nextLine();
+        System.out.println(name);
+        
+        System.out.print("Input your age: ");
+        int age=sc.nextInt();
+        System.out.println(age);
+        
+        System.out.println("Enter ur motto: ");
+        String motto = sc.nextLine();
+        System.out.println(motto);
+
+        System.out.print("Enter your salary: ");
+        float salary = sc.nextFloat();
+        System.out.println(salary);
+
+    }
+}
